e3f99446d7d0c26578cc3250e097d4cf062c8d07
b33c00l <psakalauskas@gmail.com>
2018-03-08 14:29:14 +0200

    contacts

---
+++ contacts.php
@@ -0,0 +1,61 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<meta charset="utf-8">
+		<meta http-equiv="X-UA-Compatible" content="IE=edge">
+		<title>Contacts</title>
+		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta/css/bootstrap.min.css" integrity="sha384-/Y6pD6FV/Vv2HJnA6t+vslU6fwYXjCFtcEpHbNJ0lyAFsXTsjBbfaDjzALeQsN6M" crossorigin="anonymous">
+		<script defer src="https://use.fontawesome.com/releases/v5.0.8/js/all.js"></script>
+		<link rel="stylesheet" href="css/style.css">
+		
+	</head>
+	<body>
+		<div class="container">
+			<div class="row justify-content-center m-5">
+				<h4>Contacts</h4>
+			</div>
+			<div class="row">
+				
+				<div class="col-md-6">
+					<div>
+						<p>Gamestar Holding OÜ</p>
+					</div>
+					<div>
+						<p>Mustamäe tee 44, c-korpus</p>
+					</div>
+					<div>
+						<p>Tallinn 10621</p>
+					</div>
+					<div>
+						<p style="font-size: 20px;">Telefon: +372 5307 6779<i class="fas fa-phone"></i></p>
+					</div>
+					<div>
+						<p>info@gamestar.ee</p>
+					</div>
+					<div>
+						<p>Oleme avatud E-R 9-17</p>
+					</div>
+					<div>
+						<p>Reg nr: 11741610</p>
+					</div>
+					<div>
+						<p>VAT nr: EE101338914</p>
+					</div>
+					<div>
+						<p>Konto nr: EE812200221048420120 Swedbank</p>
+					</div>
+					<div>
+						<p>Konto nr: EE971010220108789019 SEB</p>
+					</div>
+				</div>		
+			
+			<div class="col-md-6">
+				<img class="map" src="images/map.png">
+			</div>
+			</div>
+			
+		</div>
+	</div>
+	
+</body>
+</html>
